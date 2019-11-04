e7215a3ba00decdecf28e2a656a01b1e90eb41cb
Donner Hanson <donnerhansoncomposer@yahoo.com>
2019-11-04 11:53:00 -0800

    updated and finished username.sh

--- username.sh
+++ username.sh
@@ -1,11 +1,13 @@
-#! /bin/username
+#! /bin/bash
 # username.sh
 # Donner Hanson
-echo "Enter a name containing lower case letters, digits, underscore character at least 3 but no more than 12 characters in length: "
-read USERNAMEIN
-while echo "$USERNAMEIN" | egrep -v "^[0-9]{3-12}" > /dev/null 2>&1
+
+echo "Enter a name containing lower case letters (first index mandatory), digits,"
+echo " underscore character and at least 3 but no more than 12 characters."
+read NAME
+while echo "$NAME" | egrep -v "^[a-z][a-z0-9_]{2,11}" > /dev/null 2>&1
 do
-	echo "lower case letters, digits, underscore - between 3-12 characters."
-	read USERNAMEIN
+	echo "lower case letters (first index mandatory), digits, underscore only- lenth 3-12: "
+	read NAME
 done
 echo "Thank you"
