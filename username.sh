607dd89239bab958f6d61e43a1791c9bae55c733
Donner Hanson <donnerhanson@yahoo.com>
2019-11-11 10:11:03 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -7,7 +7,7 @@ echo " underscore character and at least 3 but no more than 12 characters."
 read NAME
 while echo "$NAME" | egrep -v "^[a-z][a-z0-9_]{2,11}" > /dev/null 2>&1
 do
-	echo "lower case letters (first index mandatory), digits, underscore only- lenth 3-12: "
+	echo "lower case letters (first index mandatory), digits, underscore only- length 3-12: "
 	read NAME
 done
 echo "Thank you"
